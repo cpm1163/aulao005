595bef7bbcac360cf131a46f97a2c34256d984e5
Carlos Mahet <carlos.mahet@gmail.com>
2021-12-08 14:25:39 -0300

    H2 database

--- src/main/java/com/devsuperior/myfirstproject/entities/Product.java
+++ src/main/java/com/devsuperior/myfirstproject/entities/Product.java
@@ -1,14 +1,28 @@
 package com.devsuperior.myfirstproject.entities;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Product {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Product implements Serializable{
+	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Double price;
 	
 	//relacionamento (1 para muitos) com a Product x Category, composição de objetos
+	@ManyToOne
+	@JoinColumn(name = "category_id")
 	private Category category;
 	
 	public Product() {
